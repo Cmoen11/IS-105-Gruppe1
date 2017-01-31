cccc3765a0330718185bebd6b6116ff7e7447baa
brynjar.blomvik <brynjar.blomvik@gmail.com>
2017-01-31 12:45:47 +0100

    nytt go prosjekt og license

---
+++ Go/log-test/log/log.go
@@ -0,0 +1,16 @@
+package log
+
+import "fmt"
+import "math"
+
+func Utregning() {
+
+  fmt.Println("Lise: ")
+    fmt.Println(math.Log2(8/4))
+    fmt.Println("Per: ")
+      fmt.Println(math.Log2(8/5))
+      fmt.Println("Oscar: ")
+        fmt.Println(math.Log2(8/3))
+        fmt.Println("Louise: ")
+          fmt.Println(math.Log2(8/1))
+}
---
+++ Go/log-test/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+  "fmt"
+  "./log"
+)
+
+func main() {
+  log.Utregning()
+  fmt.Println("utregning fullført")
+}
