bac8e6bed3b95d308da00bd7a663e5c612db3d3d
HelleTrygsland <hellle_trygsland@hotmail.com>
2017-01-31 11:17:17 +0100

    oppgaveløsninge log

---
+++ log/log.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println(math.Log2(2))
+	fmt.Println(math.Log2(8 / 4))
+	fmt.Println(math.Log2(8 / 5))
+	fmt.Println(math.Log2(8 / 3))
+	fmt.Println(math.Log2(8 / 7))
+	fmt.Println(math.Log2(8 / 1))
+
+}
