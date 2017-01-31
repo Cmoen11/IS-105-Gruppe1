3a0038d0ef3627159473e83a68a515ed57fed053
Erlend W <zw@zwirc.com>
2017-01-31 12:35:08 +0100

    Oppgave løsninger

---
+++ Oppgaver/GoLang/log/log.go
@@ -0,0 +1,8 @@
+package log
+
+import (
+	"fmt")
+
+func Oppgave() {
+	fmt.Println("Log under /log log mappa ")
+}
---
+++ Oppgaver/GoLang/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+  "./log"
+)
+func main() {
+	fmt.Println("Hello, playground")
+	// Kalle public Oppgave i Log.go i samme mappe:
+		Oppgave()
+	// Log under ./log mappen:
+	//log.Oppgave()
+	
+}
